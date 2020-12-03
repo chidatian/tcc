60edeb895ccd68e86a56d237fa6c690882981d30
tianchida <chidatian@126.com>
2020-12-03 19:56:44 +0800

    rpc

--- controllers/RpcController.php
+++ controllers/RpcController.php
@@ -9,10 +9,14 @@ use Tc\Lib\Assert;
 
 class RpcController extends Controller {
     public function index() {
+        
+        $this->response->successJson('test');
+    }
+
+    public function err() {
         LogHelper::write('d:/keke/log/test.log', 'test');
         LogHelper::error('d:/keke/log/test.log');
     	Assert::operateError(0);
         ErrHandler::throw(Err::create(ErrCode::NOT_FOUND_ROUTER));
-        $this->response->successJson('test');
     }
 }
